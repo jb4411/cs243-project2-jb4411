ed3f235645192dba9105c360bee1047fca986399
Jesse  Burdick-Pless <jb4411@cs.rit.edu>
2020-11-10 12:50:20 -0500

    Programmed mr_encode, mr_decode, and bagn work on mr_encrypt.

--- mirsa_lib.c
+++ mirsa_lib.c
@@ -9,6 +9,7 @@
 #include <stdint.h>
 #include <errno.h>
 #include <assert.h>
+#include <math.h>
 
 /// Used to indicate verbose mode.
 
@@ -25,8 +26,8 @@ bool mr_verbose( bool value ) {
 /// Make public and private key values and key files.
 
 bool mr_make_keys( uint64_t p, uint64_t q, const char * user ) {
-	unsigned long int n = p * q;
-	unsigned long int phi = (p - 1) * (q - 1);
+	uint64_t n = p * q;
+	uint64_t phi = (p - 1) * (q - 1);
 
 	if( verbose ) {
 		printf("Prime p = %ld\n", p);
@@ -36,12 +37,12 @@ bool mr_make_keys( uint64_t p, uint64_t q, const char * user ) {
 	}
 
 	int e = 2;
-	unsigned long int d = 0;
+	uint64_t d = 0;
 	bool done = 0;
 
-	unsigned long int tmp = 0, quotient = 0;
-	unsigned long int old_r = e, r = phi;
-	unsigned long int old_s = 1, s = 0, old_t = 0, t = 1;
+	uint64_t tmp = 0, quotient = 0;
+	uint64_t old_r = e, r = phi;
+	uint64_t old_s = 1, s = 0, old_t = 0, t = 1;
 
 	while( e < 9 && !done ) {
 		e++;
@@ -162,4 +163,127 @@ key_t * mr_read_keyfile( const char * file_name ) {
 	}
 
 	return key;
+}
+
+uint64_t mr_encrypt( uint64_t p, const key_t * pubkey) {
+	if( p >= pubkey->nonce ) {
+		fprintf( stderr, "error: mr_encrypt: code overflow.\n" );
+		fprintf( stderr, "error: mr_encrypt: limit exceeded by code value: %lu.\n", p );
+		exit( EXIT_FAILURE );
+	}
+	uint64_t e = pubkey->key;
+	uint64_t n = pubkey->nonce;
+	uint64_t C = 1;
+	uint64_t power = e;
+	uint64_t step = 1;
+	uint64_t num = 1;
+
+	// encryption math
+	if( power > 0 ) {
+		if( power % 2 ) {
+			C = p % n;
+			power--;
+		}
+		while( power ) {
+			num = 1;
+			step = 1;
+			while( step < power ) {
+				num *= (p*p) % n;
+				num %= n;
+				step *= 2;
+				power -= step;
+			}
+			C *= num;
+			C %= n;
+		}
+	}
+	return C;
+}
+
+uint64_t mr_decrypt( uint64_t c, const key_t * pvtkey) { 
+	if( c >= pvtkey->nonce ) {
+		fprintf( stderr, "error: mr_decrypt: cipher overflow.\n" );
+		exit( EXIT_FAILURE );
+	}
+	return 0;
+}
+
+uint64_t mr_encode( const char * st) {
+	int len = strlen(st);
+	if( verbose ) {
+		printf("message <%s>:\t", st);
+		printf("len%d OK: ", len);
+		if(len > 4) {
+			printf("FALSE\n");
+		} else {
+			printf("TRUE\t");
+		}
+	}
+	if(len > 4) {
+		fprintf( stderr, "error: mr_encode: could not convert '%s'.\n", st);
+		exit( EXIT_FAILURE );
+	}
+	int val_ok = 1;
+	char hex[17];
+	char tmp[4];
+	char *rst;
+	int i = 0, j = 0;
+	while(st[i]) {
+		sprintf(tmp, "%x", (int) st[i]);
+		hex[j++] = tmp[0];
+		hex[j++] = tmp[1];
+		i++;
+		if(strtol(tmp, &rst, 16) > strtol("FF", &rst, 16)) {
+			val_ok = 0;
+		}
+	}
+	if( verbose ) {
+		printf("valOK: ");
+		if(val_ok) {
+			printf("TRUE\t");
+		} else {
+			printf("FALSE\n");
+		}
+	}
+	while(j < 17) {
+		hex[j] = '\0';
+		j++;
+	}
+	uint64_t result = strtol(hex, &rst, 16);
+	if(!val_ok) {
+		fprintf( stderr, "error: mr_encode: could not convert '%s'.\n", st);
+		exit( EXIT_FAILURE );
+	}
+	if( verbose ) {
+		printf("code: <%lu> 0x%s\n", result, hex);
+	}
+
+	return result;
+}
+
+char * mr_decode( uint64_t code) {
+	double bits = 0;
+	bits = log2(code); 
+	bits = ceil(bits);
+	bits = bits/8;
+	int size = ceil(bits);
+
+	char *str = calloc(size + 1, sizeof(char));
+	assert(str != NULL);
+
+	uint64_t bitmask = 255;
+	int i = 0;
+	while(i < size) {
+		str[i] = (char) ((code >> (8 * i)) & bitmask);
+		i++;
+	}
+
+	char *result = calloc(size + 1, sizeof(char));
+	assert(result != NULL);
+	int j = 0, k = size-1;
+	while( j < size ) {
+		result[k--] = str[j++];
+	}
+	free(str);
+	return result;
 }
